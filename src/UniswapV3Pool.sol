03a91cc833136cb1d329eefc9a4a1961aead7f3a
khubak <hubakk@gmail.com>
2026-09-22 11:26:28 +0200

    WIP: UniswapV3Pool.sol/UniswapV3Pool.t.sol

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -1,3 +1,126 @@
 pragma solidity ^0.8.14;
 
-contract UniswapV3Pool {}
+import "src/lib/Tick.sol";
+import "src/lib/Position.sol";
+
+contract UniswapV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    // Pool tokens, immutable
+    address public immutable token0;
+    address public immutable token1;
+
+    // Packing variables that are read together
+    struct Slot0 {
+        // Current sqrt(P)
+        uint160 sqrtPriceX96;
+        // Current tick
+        int24 tick;
+    }
+    Slot0 public slot0;
+
+    // Amount of liquidity, L.
+    uint128 public liquidity;
+
+    // Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+    // Positions info
+    mapping(bytes32 => Position.Info) public positions;
+
+    constructor(
+        address _token0,
+        address _token1,
+        uint160 sqrtPriceX96,
+        int24 tick
+    ) {
+        token0 = _token0;
+        token1 = _token1;
+
+        slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick});
+    }
+
+    function mint(
+        address owner,
+        int24 lowerTick,
+        int24 upperTick,
+        uint128 amount
+    ) external returns (uint256 amount0, uint256 amount1) {
+        if (
+            lowerTick >= upperTick ||
+            lowerTick < MIN_TICK ||
+            upperTick > MAX_TICK
+        ) revert InvalidTickRange();
+        if (amount == 0) revert ZeroLiquidity();
+
+        uint256 balance0Before;
+        uint256 balance1Before;
+
+        ticks.update(lowerTick, amount);
+        ticks.update(upperTick, amount);
+
+        Position.Info storage position = positions.get(
+            owner,
+            lowerTick,
+            upperTick
+        );
+
+        position.update(amount);
+
+        amount0 = 0.998976618347425280 ether;
+        amount1 = 5000 ether;
+
+        if (amount0 > 0) balance0Before = balance0();
+        if (amount1 > 1) balance1Before = balance1();
+
+        IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(
+            amount0,
+            amount1
+        );
+
+        if (amount0 > 0 && balance0Before + amount0 > balance0()) {
+            revert InsufficientInputAmount();
+        }
+        if (amount1 > 0 && balance1Before + amount1 > balance1()) {
+            revert InsufficientInputAmount();
+        }
+
+        emit Mint(
+            msg.sender,
+            owner,
+            lowerTick,
+            upperTick,
+            amount,
+            amount0,
+            amount1
+        );
+    }
+
+    function balance0() internal returns (uint256 balance) {
+        balance = IERC20(token0).balanceOf(address(this));
+    }
+
+    function balance1() internal returns (uint256 balance) {
+        balance = IERC20(token0).balanceOf(address(this));
+    }
+
+    function update(
+        mapping(int24 => Tick.Info) storage self,
+        int24 tick,
+        uint128 liquidityDelta
+    ) internal {
+        Tick.Info storage tickInfo = self[tick];
+        uint128 liquidityBefore = tickInfo.liquidity;
+        uint128 liquidityAfter = liquidityBefore + liquidityDelta;
+
+        if (liquidityBefore == 0) {
+            tickInfo.initialized = true;
+        }
+
+        tickInfo.liquidity = liquidityAfter;
+    }
+}
